95c4216f6039b3bf4755fbdd68967cc71cd386e6
geor0014 <geor0014@hz.nl>
2024-05-17 11:12:03 +0900

    2 - set step state

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const messages = [
   "Learn React ⚛️",
   "Apply for jobs 💼",
@@ -5,7 +7,17 @@ const messages = [
 ];
 
 export default function App() {
-  const step = 1;
+  const [step, setStep] = useState(1);
+
+  function handlePrevious() {
+    if (step === 1) return;
+    setStep(step - 1);
+  }
+
+  function handleNext() {
+    if (step === 3) return;
+    setStep(step + 1);
+  }
   return (
     <div className="steps">
       <div className="numbers">
@@ -13,15 +25,18 @@ export default function App() {
         <div className={`${step >= 2 ? "active" : ""}`}>2</div>
         <div className={`${step >= 3 ? "active" : ""}`}>3</div>
       </div>
+
       <p className="message">
         step {step}: {messages[step - 1]}
       </p>
+
       <div className="buttons">
         <button
           style={{
             backgroundColor: "#7950f2",
             color: "#fff",
           }}
+          onClick={handlePrevious}
         >
           Previous
         </button>
@@ -30,6 +45,7 @@ export default function App() {
             backgroundColor: "#7950f2",
             color: "#fff",
           }}
+          onClick={handleNext}
         >
           Next
         </button>
